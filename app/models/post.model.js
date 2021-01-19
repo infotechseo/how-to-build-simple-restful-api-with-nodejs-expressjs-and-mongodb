9c4c9d623988ec9141ec0e81647c3e208e9bca64
ved prakash maurya <vedmaurya@gmail.com>
2021-01-19 13:53:12 +0530

    first commit

---
+++ app/models/post.model.js
@@ -0,0 +1,14 @@
+/**
+ * model
+ * author: seoinfotech
+ */
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+    title: String,
+    content: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Post', PostSchema);
---
+++ config/database.config.js
@@ -0,0 +1,7 @@
+/**
+ * database config
+ * author: seoinfotech
+ */
+module.exports = {
+    url: 'mongodb://localhost:27017/si-posts'
+}
